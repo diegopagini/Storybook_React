b9e363647bf2cf2499f7170a2d5f8a932fe48e66
Diego Pagini <diegopaginidev@gmail.com>
2022-12-03 19:56:39 +0100

    doc

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,6 +1,18 @@
 /** @format */
 import './mylabel.css';
 
-export const MyLabel = () => {
-	return <span>Hola Mundo</span>;
+type Size = 'normal' | 'h1' | 'h2' | 'h3';
+export interface Props {
+	/**
+	 * Este es el texto a mostrar en la etiqueta.
+	 */
+	label: string;
+	/**
+	 * Este es el tamaño de la etiqueta.
+	 */
+	size: Size;
+}
+
+export const MyLabel = ({ label = 'No Label', size = 'normal' }: Props) => {
+	return <span className={`${size}`}>{label}</span>;
 };
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,12 +1,18 @@
 /** @format */
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
 import { MyLabel } from '../../components/MyLabel';
 
 // 1 step.
 export default {
 	title: 'UI/MyLabel',
 	component: MyLabel,
-};
+} as ComponentMeta<typeof MyLabel>;
 // 2 step.
-const Template = () => <MyLabel />;
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />;
 // 3 step.
 export const Basic = Template.bind({});
+Basic.args = {
+	label: 'MyLabel',
+	size: 'normal',
+};
